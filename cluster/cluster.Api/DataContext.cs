776b9256d4b2c3e9a2009603eb012b1348312263
JEETVP <roberto.villegasrvo@gmail.com>
2024-11-26 11:27:01 -0600

    creacion clase data context

---
+++ cluster/cluster.Api/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using cluster.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace cluster.Api
+{
+    public class DataContext : DbContext
+    {
+        public DbSet <City> Cities { get; set; }
+    }
+}
