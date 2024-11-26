41b21f2090194c44c435bb0a69cb5284a8e85681
JEETVP <roberto.villegasrvo@gmail.com>
2024-11-26 08:51:56 -0600

    creacion del modelo, clases ciudad y pais

---
+++ cluster/cluster.Shared/Entities/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cluster.Shared.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [Display (Name = "Ciudad")]
+        public string Name { get; set; }
+    }
+}
---
+++ cluster/cluster.Shared/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cluster.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [Display(Name = "Pais")]
+        public string Name { get; set; }
+    }
+}
